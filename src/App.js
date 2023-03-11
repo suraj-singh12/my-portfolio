718363ef1936f76cf76e791cb1608a9b8ce543d5
suraj-singh12 <surajsingh5092@gmail.com>
2023-03-11 08:48:24 +0530

    download button css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Spline from "@splinetool/react-spline";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
-import ProfilePhoto from './images/suraj.jpg';
+import ProfilePhoto from "./images/suraj.jpg";
 
 function App() {
   const [isActive, setIsActive] = useState(false);
@@ -114,25 +114,61 @@ function App() {
 
         <div className="absolute bottom-0 w-full justify-center items-center flex">
           <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl"></div>
-        <p className="text-textBase">Press and drag to orbit</p>
+          <p className="text-textBase">Press and drag to orbit</p>
         </div>
       </div>
-
 
       {/* Main Section */}
       <main className="w-[80%] mt-5">
         {/* About section */}
-        <section className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24" id="#about">
+        <section
+          className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 my-24"
+          id="#about"
+        >
           {/* image box*/}
           <div className="w-full h-420 flex items-center justify-center">
             <div className="w-275 h-340 bg-emerald-200 rounded-md relative">
-              <img className="w-full h-full absolute -right-4 top-4 object-cover
-              rounded-lg drop-shadow-2xl " src={ProfilePhoto} alt="user profile" />
+              <img
+                className="w-full h-full absolute -right-4 top-4 object-cover
+              rounded-lg shadow-lg"
+                src={ProfilePhoto}
+                alt="user profile"
+              />
             </div>
           </div>
-          {/* content box */}
-          <div></div>
 
+          {/* content box */}
+          <div className="w-full h-420 flex flex-col items-center justify-center">
+            <p className="text-lg text-textBase text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              incidunt, odio asperiores dolorem rem praesentium consequuntur
+              minima eaque quis. Magni tenetur cumque necessitatibus porro esse
+              suscipit rem, vel animi nobis! Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Illum perspiciatis laborum,
+              recusandae repellat eius ex et repellendus itaque incidunt error
+              eligendi, molestiae libero qui tempora quae, inventore possimus
+              natus nulla.
+            </p>
+          </div>
+
+          
+          <button
+            className="w-full md:w-auto relative mt-6 inline-flex items-center 
+          justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium 
+          text-gray-900 rounded-lg group bg-gray-900 bg-gradient-to-br from-green-400 to-blue-600 
+          group-hover:from-green-400 group-hover:to-blue-600 hover:text-white 
+          dark:text-white focus:ring-4 focus:ring-green-200 
+          dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg 
+          hover:dark:shadow-teal-800/80"
+          >
+            <span
+              className="w-full relative px-5 py-2.5 
+            transition-all ease-in duration-75 bg-white dark:bg-gray-900 
+            rounded-md group-hover:bg-opacity-0"
+            >
+              Download
+            </span>
+          </button>
         </section>
       </main>
     </div>
