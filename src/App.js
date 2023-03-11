b63af572dc24ddb142a912f7b13df30152d8533a
suraj-singh12 <surajsingh5092@gmail.com>
2023-03-11 08:50:16 +0530

    button properly aligned

--- src/App.js
+++ src/App.js
@@ -149,26 +149,25 @@ function App() {
               eligendi, molestiae libero qui tempora quae, inventore possimus
               natus nulla.
             </p>
-          </div>
 
-          
-          <button
-            className="w-full md:w-auto relative mt-6 inline-flex items-center 
-          justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium 
-          text-gray-900 rounded-lg group bg-gray-900 bg-gradient-to-br from-green-400 to-blue-600 
-          group-hover:from-green-400 group-hover:to-blue-600 hover:text-white 
-          dark:text-white focus:ring-4 focus:ring-green-200 
-          dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg 
-          hover:dark:shadow-teal-800/80"
-          >
-            <span
-              className="w-full relative px-5 py-2.5 
-            transition-all ease-in duration-75 bg-white dark:bg-gray-900 
-            rounded-md group-hover:bg-opacity-0"
+            <button
+              className="w-full md:w-auto relative mt-6 inline-flex items-center 
+            justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium 
+            text-gray-900 rounded-lg group bg-gray-900 bg-gradient-to-br from-green-400 to-blue-600 
+            group-hover:from-green-400 group-hover:to-blue-600 hover:text-white 
+            dark:text-white focus:ring-4 focus:ring-green-200 
+            dark:focus:ring-green-800 hover:shadow-lg hover:shadow-teal-500/50 hover:dark:shadow-lg 
+            hover:dark:shadow-teal-800/80"
             >
-              Download
-            </span>
-          </button>
+              <span
+                className="w-full relative px-5 py-2.5 
+              transition-all ease-in duration-75 bg-white dark:bg-gray-900 
+              rounded-md group-hover:bg-opacity-0"
+              >
+                Download
+              </span>
+            </button>
+          </div>
         </section>
       </main>
     </div>
